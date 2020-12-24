631209514f020a58e7426706979022772eb2fc28
Jxnahs / Jonah#1337 <74617956+Jxnahs@users.noreply.github.com>
2020-12-24 16:23:33 +0000

    Update Modules.bat

--- Modules.bat
+++ Modules.bat
@@ -1,12 +1,7 @@
 pip install selenium
 pip install requests
 pip install undetected_chromedriver
-pip install pyautogui
-pip install pynput
-pip install pywin32
-pip install pyperclip
 pip install ctypes
 pip install colorama
 pip install discord_webhook
 pip install json
-pip install pyarmor
